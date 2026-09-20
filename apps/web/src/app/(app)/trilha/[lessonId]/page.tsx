fc213c5aa923ea29e2eebb73b7b2b95e86c7dbe4
Anderson Silva <anderson-psv@hotmail.com>
2026-09-20 10:51:56 -0300

    deploy page fix

--- apps/web/src/app/(app)/trilha/[lessonId]/page.tsx
+++ apps/web/src/app/(app)/trilha/[lessonId]/page.tsx
@@ -1,4 +1,9 @@
+import { mockLessonDetails } from "@/lib/mock-data";
 import { LessonPageClient } from "./LessonPageClient";
+
+export function generateStaticParams() {
+    return Object.keys(mockLessonDetails).map((lessonId) => ({ lessonId }));
+}
 
 export default function LessonPage() {
     return <LessonPageClient />;
